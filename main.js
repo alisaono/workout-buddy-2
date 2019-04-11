5c2aaf078d920b99f81eee7e4d3c6c68296b907b
flo <loflorence2@gmail.com>
2019-04-11 18:53:57 -0400

    Detect front/side plank; Start timer

--- main.js
+++ main.js
@@ -23,6 +23,23 @@ let JOINT_INDEX = {
   'RIGHT_ANKLE': 16
 }
 
+let pose = {
+  'FRONT_PLANK': 0,
+  'SIDE_PLANK': 1,
+  'PUSH_UP': 2,
+  'CRUNCH': 3,
+  'LUNGES': 4
+}
+
+FRONT_PLANK = false
+SIDE_PLANK = false
+PUSH_UP = false
+CRUNCH = false
+LUNGES = false
+
+let straight_back = 0.1
+let meh_straight = 30
+
 let video
 let poseNet
 let keypoints = []
@@ -55,6 +72,107 @@ function onModelReady() {
   console.log('Model loaded')
 }
 
+
+
+//  Simple example of using private variables
+//
+//  To start the stopwatch:
+//    obj.start();
+//
+//  To get the duration in milliseconds without pausing / resuming:
+//    var x = obj.time();
+//
+//  To pause the stopwatch:
+//    var x = obj.stop(); // Result is duration in milliseconds
+//
+//  To resume a paused stopwatch
+//    var x = obj.start();  // Result is duration in milliseconds
+//
+//  To reset a paused stopwatch
+//    obj.stop();
+//
+var clsStopwatch = function() {
+    // Private vars
+    var startAt = 0  // Time of last start / resume. (0 if not running)
+    var lapTime = 0  // Time on the clock when last stopped in milliseconds
+
+    var now = function() {
+        return (new Date()).getTime() 
+      } 
+ 
+    // Public methods
+    // Start or resume
+    this.start = function() {
+        startAt = startAt ? startAt : now()
+      }
+
+    // Stop or pause
+    this.stop = function() {
+        // If running, update elapsed time otherwise keep it
+        lapTime = startAt ? lapTime + now() - startAt : lapTime
+        startAt = 0 // Paused
+      }
+
+    // Reset
+    this.reset = function() {
+        lapTime = startAt = 0
+      }
+
+    // Duration
+    this.time = function() {
+        return lapTime + (startAt ? now() - startAt : 0)
+      }
+  }
+
+var x = new clsStopwatch()
+var $time
+var clocktimer
+
+function pad(num, size) {
+  var s = "0000" + num
+  return s.substr(s.length - size)
+}
+
+function formatTime(time) {
+  var h = m = s = 0
+  var newTime = ''
+
+  h = Math.floor( time / (60 * 60 * 1000) )
+  time = time % (60 * 60 * 1000)
+  m = Math.floor( time / (60 * 1000) )
+  time = time % (60 * 1000)
+  s = Math.floor( time / 1000 )
+
+  newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2)
+  return newTime
+}
+
+function show() {
+  $time = document.getElementById('time')
+  update()
+}
+
+function update() {
+  $time.innerHTML = formatTime(x.time())
+}
+
+function start() {
+  clocktimer = setInterval("update()", 1)
+  x.start()
+}
+
+function stop() {
+  x.stop()
+  clearInterval(clocktimer)
+}
+
+function reset() {
+  stop()
+  x.reset()
+  update()
+}
+
+
 function onPoseUpdated(poses) {
   if (poses.length === 0) {
     console.log('No one in the frame')
@@ -76,7 +194,64 @@ function onPoseUpdated(poses) {
   // For each keypoint, you can get:
   // keypoint.position.x for x coord, keypoint.position.y for y coord,
   // keypoint.score for confidence %
+
+  key_nose = keypoints[JOINT_INDEX.NOSE]
+  key_lshoulder = keypoints[JOINT_INDEX.LEFT_SHOULDER]
+  key_rshoulder = keypoints[JOINT_INDEX.RIGHT_SHOULDER]
+  key_lelbow = keypoints[JOINT_INDEX.LEFT_ELBOW]
+  key_relbow = keypoints[JOINT_INDEX.RIGHT_ELBOW]
+  key_lwrist = keypoints[JOINT_INDEX.LEFT_WRIST]
+  key_rwrist = keypoints[JOINT_INDEX.RIGHT_WRIST]
+  key_lhip = keypoints[JOINT_INDEX.LEFT_HIP]
+  key_rhip = keypoints[JOINT_INDEX.RIGHT_HIP]
+  key_lknee = keypoints[JOINT_INDEX.LEFT_KNEE]
+  key_rknee = keypoints[JOINT_INDEX.RIGHT_KNEE]
+  key_lankle = keypoints[JOINT_INDEX.LEFT_ANKLE]
+  key_rankle = keypoints[JOINT_INDEX.RIGHT_ANKLE]
+
+  neck_pos = [(key_lshoulder.position.x + key_rshoulder.position.x) / 2, (key_lshoulder.position.y + key_rshoulder.position.y) / 2]
+
+
+  // if ((abs(key_lshoulder.position.x - key_lelbow.position.x) <= 30 ||
+  //     abs(key_rshoulder.position.x - key_relbow.position.x) <= 30) &&
+  //   (abs(key_lelbow.position.y - key_lwrist.position.y) <= 30 ||
+  //     abs(key_relbow.position.y - key_rwrist.position.y) <= 30 ) &&
+  //   (abs(key_nose.position.y - key_lshoulder.position.y) <= 50 ||
+  //     abs(key_nose.position.y - key_rshoulder.position.y) <= 50) &&
+  //   (abs((key_rshoulder.position.y - key_rknee.position.y) / (key_rshoulder.position.x - key_rknee.position.x)) <= straight_back ||
+  //     abs((key_lshoulder.position.y - key_lknee.position.y) / (key_lshoulder.position.x - key_lknee.position.x)) <= straight_back) 
+  //   ) {
+  //   FRONT_PLANK = true
+  //   start()
+  // }
+  // else{
+  //   stop()
+  // }
+
+
+
+  if (abs(key_lshoulder.position.x - key_lelbow.position.x) <= 30 &&
+      abs(key_rshoulder.position.x - key_relbow.position.x) <= 30 &&
+      abs(key_lelbow.position.x - key_lwrist.position.x) <= 30 &&
+      abs(key_relbow.position.x - key_rwrist.position.x) <= 30 &&
+    (abs(key_nose.position.y - key_lshoulder.position.y) <= 50 ||
+      abs(key_nose.position.y - key_rshoulder.position.y) <= 50) &&
+    (abs((neck_pos[1] - key_rknee.position.y) / (neck_pos[0] - key_rknee.position.x)) <= 4 ||
+      abs((neck_pos[1] - key_lknee.position.y) / (neck_pos[0] - key_lknee.position.x)) <= 4) 
+    ) {
+    SIDE_PLANK = true
+    start()
+  }
+  // else{
+  //   stop()
+  // }
+
+
 }
+
+
+
+
 
 function drawKeypoints() {
   for (let keypoint of keypoints) {
